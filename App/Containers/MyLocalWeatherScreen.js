d5cd7abc00c8f31dab57da7d07ed5cbd7ca4a765
Larry Mahony <larry.mahony@gmail.com>
2017-11-12 09:47:26 +0000

    Displaying the 3 day forecast

--- App/Containers/MyLocalWeatherScreen.js
+++ App/Containers/MyLocalWeatherScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, ScrollView, Text, KeyboardAvoidingView, ActivityIndicator, Image, Linking } from 'react-native'
+import { View, ScrollView, Text, KeyboardAvoidingView, ActivityIndicator, Image, Linking, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 import WeatherActions from '../Redux/WeatherRedux'
@@ -13,18 +13,44 @@ class MyLocalWeatherScreen extends Component {
     if (!this.props.current.temp_c) {
       this.props.getCurrent()
     }
-  /*  if (!this.props.forecast.txt_forecast) {
+    if (!this.props.forecast.txt_forecast) {
       this.props.getForecast();
-    }*/
+    }
 
 
   }
+
+  _FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#AAA",
+        }}
+      />
+    );
+  }
+
+  _renderForecastItem = ({item, index}) => {
+    return (
+      <View>
+        <Text>{item.title}</Text>
+        <Text>{item.fcttext_metric}</Text>
+      </View>
+    )
+  }
+
 
   render () {
     const spinner = this.props.fetching ? <ActivityIndicator size='large'/> : null;
     let iconUrl = 'https://icons.wxug.com/i/c/k/nt_cloudy.gif'
     if (this.props.current.icon_url) {
       iconUrl = this.props.current.icon_url.replace('http:', 'https:')
+    }
+    let forecastMain = {}
+    if (this.props.forecast && this.props.forecast.txt_forecast) {
+      forecastMain = this.props.forecast.txt_forecast
     }
     return (
       <ScrollView style={{padding: 4}}>
@@ -49,7 +75,13 @@ class MyLocalWeatherScreen extends Component {
           <Text>{this.props.error}</Text>
 
           <View>
-            <Text>Forecast</Text>
+            <Text>Forecast </Text>
+            <FlatList
+              data={forecastMain.forecastday}
+              renderItem={this._renderForecastItem}
+              keyExtractor={(item, index) => index}
+              ItemSeparatorComponent = {this._FlatListItemSeparator}
+            />
           </View>
           {spinner}
         </KeyboardAvoidingView>
