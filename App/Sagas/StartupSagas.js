9a76c42d9a0d75f4d41d01783e4eff9a652d09b0
Larry Mahony <larry.mahony@gmail.com>
2017-11-17 13:34:55 +0000

    more tidy up

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -1,5 +1,4 @@
 import { put, select } from 'redux-saga/effects'
-import GithubActions from '../Redux/GithubRedux'
 import { is } from 'ramda'
 
 // exported to make available for tests
@@ -8,27 +7,7 @@ import { is } from 'ramda'
 // process STARTUP actions
 export function * startup (action) {
   if (__DEV__ && console.tron) {
-    // straight-up string logging
-    console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
 
-    // logging an object for better clarity
-    console.tron.log({
-      message: 'pass objects for better logging',
-    })
-
-    // fully customized!
-    const subObject = { a: 1, b: [1, 2, 3], c: true }
-    subObject.circularDependency = subObject // osnap!
-    console.tron.display({
-      name: '🔥 IGNITE 🔥',
-      preview: 'You should totally expand this',
-      value: {
-        '💃': 'Welcome to the future!',
-        subObject,
-        someInlineFunction: () => true,
-        someGeneratorFunction: startup
-      }
-    })
   }
 
 }
